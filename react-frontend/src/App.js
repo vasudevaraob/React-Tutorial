2f9b00639ca91027d998762b1e7f6d483e783529
vasudevaraob <29686823+vasudevaraob@users.noreply.github.com>
2021-03-18 17:23:59 +0530

    Developed front end React curd Application

---
+++ react-frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent'
+import ViewEmployeeComponent from './components/ViewEmployeeComponent'
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent'
+
+function App() {
+  return (
+    <div>
+      <Router>
+              <HeaderComponent />
+                <div className="container">
+                    <Switch> 
+                          <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+                          <Route path = "/employees" component = {ListEmployeeComponent}></Route>
+                          <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
+                          <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}></Route>
+                          {/* <Route path = "/update-employee/:id" component = {CreateEmployeeComponent}></Route> */}
+                    </Switch>
+                </div>
+              <FooterComponent />
+        </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-frontend/src/components/CreateEmployeeComponent.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react'
+import EmployeeService from '../services/EmployeeService'
+
+class CreateEmployeeComponent extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            id: this.props.match.params.id,
+            firstName: '',
+            lastName: '',
+            emailId: ''
+        }
+        this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this)
+        this.changeLastNameHandler = this.changeLastNameHandler.bind(this)
+        this.changeEmailNameHandler = this.changeEmailNameHandler.bind(this)
+       // this.saveEmployee = this.saveEmployee.bind(this)
+        this.saveOrUpdateEmployee = this.saveOrUpdateEmployee.bind(this);
+    }
+
+    componentDidMount(){
+
+        if(this.state.id ==='_add'){
+            return
+        }
+        else{
+
+            EmployeeService.getEmployeeById(this.state.id).then(
+                (res) => {
+                    let employee = res.data
+                    this.setState({firstName: employee.firstName,lastName: employee.lastName,emailId: employee.emailId})
+                }
+            )
+        }
+
+    }
+
+    saveOrUpdateEmployee(e){
+            e.preventDefault();
+            let employee = {firstName: this.state.firstName,lastName: this.state.lastName,emailId:this.state.emailId}
+            console.log('employee => '+JSON.stringify(employee))
+
+            if(this.state.id === '_add'){
+                EmployeeService.createEmployee(employee).then(
+                             (res) => {
+                                 this.props.history.push('/employees');
+                                 console.log('employee => '+JSON.stringify(employee))
+                             } 
+                       )
+            } else {
+                EmployeeService.updateEmployee(employee, this.state.id).then( res => {
+                    this.props.history.push('/employees');
+                    console.log('employee => '+JSON.stringify(employee))
+                });
+            }
+
+    }
+
+
+    // saveEmployee(e){
+    //     e.preventDefault();
+    //     let employee = {firstName: this.state.firstName,lastName: this.state.lastName,emailId:this.state.emailId}
+    //     console.log('employee => '+JSON.stringify(employee))
+    //     EmployeeService.createEmployee(employee).then(
+    //         (res) => {
+    //             this.props.history.push('/employees');
+    //         } 
+    //     )
+    // }
+
+    cancel(){
+        this.props.history.push('/employees');
+    }
+
+    changeFirstNameHandler= (event) => {
+        this.setState({firstName: event.target.value});
+    }
+    changeLastNameHandler= (event) => {
+        this.setState({lastName: event.target.value});
+    }
+    changeEmailNameHandler= (event) => {
+        this.setState({emailId: event.target.value});
+    }
+    getTitle(){
+        if(this.state.id === '_add'){
+            return <h3 className="text-center">Add Employee</h3>
+        }else{
+            return <h3 className="text-center">Update Employee</h3>
+        }
+    }
+        
+    render() {
+        return (
+            <div>
+                <div className='container'>
+                    <div className='row'>
+                        <div className="card col-md-6 offset-md-3 offset-md-3">
+                                 {
+                                    this.getTitle()
+                                }
+                            <div className='card-body'>
+                                <form>
+                                    <div className='form-group'>
+                                        <label>First Name</label>
+                                        <input placeholder="First Name" className='form-control' name='firstName' value={this.state.firstName} onChange={this.changeFirstNameHandler} ></input>
+                                    </div>
+                                    <div className='form-group'>
+                                        <label>Last Name</label>
+                                        <input placeholder="Last Name" className='form-control' name='lastName' value={this.state.lastName} onChange={this.changeLastNameHandler} ></input>
+                                    </div>
+                                    <div className='form-group'>
+                                        <label>Email ID</label>
+                                        <input placeholder="Email ID" className='form-control' name='emailId' value={this.state.emailId} onChange={this.changeEmailNameHandler} ></input>
+                                    </div>
+                                    <button className='btn btn-success' onClick={this.saveOrUpdateEmployee}>Save</button>
+                                    <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CreateEmployeeComponent
